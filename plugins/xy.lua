985d72d0d7fe00adeb70019c90d4b6496ec67853
mohamadkhoshnava <mohamadkhoshnava@users.noreply.github.com>
2016-03-06 10:06:00 +0330

    Create xy.lua

---
+++ plugins/xy.lua
@@ -0,0 +1,32 @@
+do
+local function run(msg, matches)
+local bot_id = 158877128
+local x = 188548712
+local y = 76798631
+local z = 152202044
+local w = 183976744
+
+    if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
+    end
+     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(y) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..y, ok_cb, false)
+    end
+     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(z) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..z, ok_cb, false)
+    end
+     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(w) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..w, ok_cb, false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^[!/](bye)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
